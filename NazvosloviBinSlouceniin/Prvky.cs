b6055bd8dadf0fd18be135c7c37e6704b7671368
Vendelín <vendelinkukla@gmail.com>
2022-02-10 20:09:58 +0100

    .

---
+++ NazvosloviBinSlouceniin/Prvky.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Prvky
+{
+    public void Listy()
+    {
+        var prvky = new Dictionary<string, string>(){
+            {"H", "Vodík"},
+            {"He", "Helium"},
+            {"Li", "Lithium"},
+            {"Be", "Beryllium"},
+            {"B", "Bor"},
+            {"C", "Uhlík"},
+            {"N", "Dusík"},
+            {"O", "Kyslík"},
+            {"F", "Fluor"},
+            {"Ne", "Neon"},
+            {"Na", "Sodík"},
+            {"Mg", "Hořčík"},
+            {"Al", "Hliník"},
+            {"Si", "Křemík"},
+            {"P", "Fosfor"},
+            {"S", "Síra"},
+            {"Cl", "Chlor"},
+            {"Ar", "Argon"},
+            {"K", "Draslík"},
+            {"Ca", "Vápník"},
+            {"Sc", "Skandium"},
+            {"Ti", "Titan"},
+            {"V", "Vanad"},
+            {"Cr", "Chrom"},
+            {"Mn", "Mangan"},
+            {"Fe", "Železo"},
+            {"Co", "Kobalt"},
+            {"Ni", "Nikl"},
+            {"Cu", "Měď"},
+            {"Zn", "Zinek"},
+            {"Ga", "Gallium"},
+            {"Ge", "Germanium"},
+            {"As", "Arsen"},
+            {"Se", "Selen"},
+            {"Br", "Brom"},
+            {"Kr", "Krypton"},
+            {"Rb", "Rubidium"},
+            {"Sr", "Stroncium"},
+            {"Y", "Yttrium"},
+            {"Zr", "Zirkonium"},
+            {"Nb", "Niob"},
+            {"Mo", "Molybden"},
+            {"Tc", "Technecium"},
+            {"Ru", "Ruthenium"},
+            {"Rh", "Rhodium"},
+            {"Pd", "Palladium"},
+            {"Ag", "Stříbro"},
+            {"Cd", "Kadmium"},
+            {"In", "Indium"},
+            {"Sn", "Cín"},
+            {"Sb", "Antimon"},
+            {"Te", "Tellur"},
+            {"I", "Jod"},
+            {"Xe", "Xenon"},
+            {"Cs", "Cesium"},
+            {"Ba", "Baryum"},
+            {"Hf", "Hafnium"},
+            {"Ta", "Tantal"},
+            {"W", "Wolfram"},
+            {"Re", "Rhenium"},
+            {"Os", "Osmium"},
+            {"Ir", "Iridium"},
+            {"Pt", "Platina"},
+            {"Au", "Zlato"},
+            {"Hg", "Rtuť"},
+            {"Tl", "Thallium"},
+            {"Pb", "Olovo"},
+            {"Bi", "Bismut"},
+            {"Po", "Polonium"},
+            {"At", "Astat"},
+            {"Rn", "Radon"},
+            {"Fr", "Francium"},
+            {"Ra", "Radium"},
+            {"Rf", "Rutherfordium"},
+            {"Db", "Dubnium"},
+            {"Sg", "Seaborgium"},
+            {"Bh", "Bohrium"},
+            {"Hs", "Hassium"},
+            {"Mt", "Meitnerium"},
+            {"Ds", "Darmstadtium"},
+            {"Rg", "Roentgenium"},
+            {"Cn", "Kopernicium"},
+            {"Nh", "Nihonium, Nihonium, Nihonium"},
+            {"Fl", "Flerovium, Flerovium, Flerovium"},
+            {"Mc", "Moscovium, Moscovium, Moscovium"},
+            {"Lv", "Livermorium, Livermorium, Livermorium"},
+            {"Ts", "Tennessin, Tennessine, Tennessine"},
+            {"Og", "Oganesson, Oganesson, Oganesson"}
+        };
+        var Soli = new Dictionary<string, string>()
+        {
+            {"F", "Flourid"},
+            {"Cl", "Chorid"},
+            {"Br", "Bromid"},
+            {"I", "Jodid"}
+        };
+
+
+
+
+    }
+}
+
+
